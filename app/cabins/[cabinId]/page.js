2b5a95c9ec875e14047dc9d87f3ee7a5d8530112
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-10-22 20:31:34 +0500

    feat: implement dynamic cabin detail page with generateStaticParams

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,4 +1,4 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -7,6 +7,14 @@ export async function generateMetadata({ params }) {
   const { name } = await getCabin(cabinId);
 
   return { title: `Cabin ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
+  return ids;
 }
 
 export default async function Page({ params }) {
@@ -33,7 +41,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="md:text-lg text-base text-primary-300 mb-10">{description}</p>
+          <p className="md:text-lg text-base text-primary-300 mb-10">
+            {description}
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
